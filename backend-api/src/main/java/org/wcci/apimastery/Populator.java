7d962fe06a169527fc289e7214d020899e63e0a5
Stuart <stumax7@gmail.com>
2020-03-21 13:57:33 -0400

    Added preliminary data to Populator

---
+++ backend-api/src/main/java/org/wcci/apimastery/Populator.java
@@ -0,0 +1,35 @@
+package org.wcci.apimastery;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class Populator implements CommandLineRunner {
+
+    @Autowired
+    private CountryStorage countryStorage;
+    @Autowired
+    private CityStorage cityStorage;
+    @Autowired
+    private AttractionStorage attractionStorage;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        Country france = new Country("France");
+        countryStorage.store(france);
+        Country italy = new Country("Italy");
+        countryStorage.store(italy);
+
+        City paris = new City("Paris", france);
+        cityStorage.store(paris);
+        City rome = new City("Rome", italy);
+        cityStorage.store(rome);
+
+        Attraction eiffel = new Attraction(paris, "Eiffel Tower" );
+        attractionStorage.store(eiffel);
+        Attraction vatican = new Attraction(rome, "Vatican");
+        attractionStorage.store(vatican);
+
+
+    }
+}
